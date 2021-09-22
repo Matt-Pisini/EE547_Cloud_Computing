684473deed2233ab6f7a0451e4d8115e93009f5e
Matt Pisini <pisini@usc.edu>
2021-09-22 11:43:37 -0700

    post functions complete

--- hw2/index.js
+++ hw2/index.js
@@ -37,7 +37,7 @@ function openFile() {
 // Creates player.json file if it does not exist
 function createFile() {
     try{
-        let json_file = {"players":[], "update_at": new Date(),"create_at":new Date(),"version": 1.0};
+        let json_file = {"players":[], "update_at": new Date(),"create_at":new Date(),"version": "1.0"};
         fs.writeFileSync('./data/player.json', JSON.stringify(json_file));
         return json_file;
     } catch(err){
@@ -72,7 +72,8 @@ function getActivePlayers() {
         let players = getPlayers();
         const active_players = [];
         for(let i = 0; i < players.length; i++) {
-            if(players[i].is_active == 1 || players[i].is_active == 't' || players[i].is_active == true) {
+            // if(players[i].is_active == 1 || players[i].is_active == 't' || players[i].is_active == true) {
+            if(players[i].is_active == true) {
                 // active_players.push(players[i]);
                 active_players.push(formatPlayerOutput(players[i]));
             }
@@ -103,16 +104,11 @@ function getPlayer(id) {
 function deletePlayer(id) {
     try{
         let json_file = openFile();
-        let file_obj = {
-            players: json_file.players,
-            update_at: json_file.update_at,
-            create_at: json_file.create_at,
-            version: json_file.version
-        }
-        for(let i = 0; i < file_obj.players.length; i++){
-            if(file_obj.players[i].pid == id){
-                let elem = file_obj.players.splice(i,1); //remove element from players array
-                reWriteFile(file_obj);
+        let players = json_file.players;
+        for(let i = 0; i < players.length; i++){
+            if(players[i].pid == id){
+                let elem = players.splice(i,1); //remove element from players array
+                reWriteFile(json_file);
                 return elem;
             }
         }
@@ -152,7 +148,7 @@ function addPlayer(params){
     }
     
     // Check initial Balance
-    if (!validateInitialBalanceInput(params.initial_balance_usd)){
+    if (!validateBalanceInput(params.initial_balance_usd) || params.initial_balance_usd == ""){
         invalid_fields.push("initial_balance_usd");
     }
 
@@ -161,7 +157,7 @@ function addPlayer(params){
         return invalid_fields;
     }
 
-    let new_player = formatPlayerData(params);
+    let new_player = formatNewPlayerData(params);
     let json_file = openFile();
     json_file.players.push(new_player);
     reWriteFile(json_file);
@@ -173,7 +169,14 @@ function addPlayer(params){
 function updatePlayer(id, query){
     let active = 0;
     if(query.active == 1 || query.active.toLowerCase() == "t" || query.active.toLowerCase() == "true"){
-        active = 1;
+        active = true;
+    }
+    else if(query.active == 0 || query.active.toLowerCase() == "f" || query.active.toLowerCase() == "false"){
+        active = false;
+    }
+    else{
+        console.log("ambiguous input for 'is_active'");
+        return 0;
     }
 
     let name_change_flag = 0;
@@ -185,29 +188,45 @@ function updatePlayer(id, query){
             return 0; //invalid lname field
         }
     }
-
-    let DNE_flag = 0;
     let json_file = openFile();
     for(let i = 0; i < json_file.players.length; i++){
         if(json_file.players[i].pid == id) {
-            DNE_flag = 1;
             json_file.players[i].is_active = active;
             if (name_change_flag){
                 json_file.players[i].lname = query.lname;
             }
+            reWriteFile(json_file);
+            return 1;
         }
     }
-    if(DNE_flag){
-        reWriteFile(json_file);
-        return 1;
-    }
-    
     return 0;
+}
+
+function updatePlayerBalance(id, query){
+    if(!validateBalanceInput(query.amount_usd) || query.amount_usd == ""){
+        return 0;
+    }
+    let new_balance = formatBalanceData(query.amount_usd)
+    let old_balance = '0.00';
+    let json_file = openFile();
+    for(let i = 0; i < json_file.players.length; i++){
+        if(json_file.players[i].pid == id) {
+            old_balance = json_file.players[i].balance_usd;
+            json_file.players[i].balance_usd = new_balance;
+            reWriteFile(json_file);
+            let player_balance = {
+                old_balance_usd: old_balance,
+                new_balance_usd: new_balance
+            };
+            return player_balance;
+        }
+    }
+    return undefined;
 }
 // *********************************************************************************
 
 // ***************************** FORMAT FUNCTIONS ***********************************
-function formatPlayerData(params){
+function formatNewPlayerData(params){
 
     let hand = '';
     if(params.handed.toLowerCase() == "left"){
@@ -220,14 +239,14 @@ function formatPlayerData(params){
         hand = "A";
     }
 
-    let balance_value = formatInitialBalance(params.initial_balance_usd);
+    let balance_value = formatBalanceData(params.initial_balance_usd);
 
     let new_player = {
         pid: next_pid,            
         fname: params.fname,
         lname: params.lname,
         handed: hand,
-        is_active: 1,
+        is_active: true,
         balance_usd: balance_value
     }
     
@@ -265,7 +284,7 @@ function formatPlayerOutput(player){
     return player_output;
 }
 
-function formatInitialBalance(balance){
+function formatBalanceData(balance){
     let money = balance.split(".");
     let balance_value = 0;
     if(money[1] == undefined){
@@ -283,7 +302,7 @@ function formatInitialBalance(balance){
 
 // ***************************** OTHER FUNCTIONS ***********************************
 
-function validateInitialBalanceInput(balance){
+function validateBalanceInput(balance){
     if(balance == undefined){
         return 0;
     }
@@ -316,6 +335,16 @@ function getStartingPID(){
     }
     return Math.max(...initial_players.map(({pid}) => pid)) + 1;
 }
+// function verifyPlayerExists(id){
+//     let json_file = openFile();
+//     for(let i = 0; i < json_file.players.length; i++){
+//         if(json_file.players[i].pid == id) {
+//                 return 1;
+//         }
+//     }
+//     return 0;
+// }
+
 // ***********************************************************************************
 
 // ***************************** EXPRESS ENDPOINTS ***********************************
@@ -387,8 +416,8 @@ app.post('/player', (req,res,next) => {
 });
 
 app.post('/player/:pid', (req,res,next) => {
-    let valid = updatePlayer(req.params.pid, req.query);
-    if(valid){
+    let status = updatePlayer(req.params.pid, req.query);
+    if(status){
         res.redirect(303, `http://${HOST}:${PORT}/player/${req.params.pid}`);
         res.end();
     }
@@ -396,7 +425,25 @@ app.post('/player/:pid', (req,res,next) => {
         res.writeHead(404);
         res.end();
     }
-    
+    next();
+});
+
+app.post('/deposit/player/:pid', (req,res,next) => {
+    let status = updatePlayerBalance(req.params.pid, req.query);
+    if(status == 0){
+        res.writeHead(400);
+        res.end();
+    }
+    else if(status == undefined)
+    {
+        res.writeHead(404);
+        res.end();
+    }
+    else{
+        res.writeHead(200);
+        res.write(JSON.stringify(status,null,2));
+        res.end();
+    }
     next();
 });
 
--- hw2/test.sh
+++ hw2/test.sh
@@ -13,10 +13,13 @@ function url() {
 # curl $CURL_OPTS -X POST $(url "/player?fname=9matt&lname=pisini&handed=right&initial_balance_usd=100")
 # curl $CURL_OPTS -X POST $(url "/player?fname=matt&lname=0pisini&handed=right&initial_balance_usd=100")
 # curl $CURL_OPTS -X POST $(url "/player?fname=matt&lname=Pisini&handed=right&initial_balance_usd=10.13")
-# curl $CURL_OPTS -X POST $(url "/player?fname=matt&lname=&handed=right&initial_balance_usd=100")
+# curl $CURL_OPTS -X POST $(url "/player?fname=matt&lname=Pisini&handed=right&initial_balance_usd=")
 
-curl $CURL_OPTS -X POST $(url "/player/10?active=0")
-curl $CURL_OPTS -X POST $(url "/player/1?active=1&lname=76yh")
+# curl $CURL_OPTS -X POST $(url "/player/1?active=0")
+# curl $CURL_OPTS -X POST $(url "/player/1?active=1&lname=76yh")
+
+curl $CURL_OPTS -X POST $(url "/deposit/player/2?amount_usd=")
+curl $CURL_OPTS -X POST $(url "/deposit/player/4?amount_usd=789")
 
 # curl $CURL_OPTS -X GET $(url "/ping")
 # curl $CURL_OPTS -X GET $(url "/player")
